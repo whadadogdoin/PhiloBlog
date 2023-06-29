5f2a7983b25473c878aca64ace5106e8f971e6e5
hp15 <abhishek.vk396@gmail.com>
2023-06-29 06:52:51 +0530

    Added drafts,publish and delete.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/',views.post_new,name='post_new'),
-    path('post/<int:pk>/edit/',views.post_edit,name='post_edit')
+    path('post/<int:pk>/edit/',views.post_edit,name='post_edit'),
+    path('drafts/',views.drafts_list,name='drafts_list'),
+    path('post/<pk>/publish/',views.post_publish,name='post_publish'),
+    path('post/<pk>/delete/',views.post_delete,name='post_delete')
 ]
